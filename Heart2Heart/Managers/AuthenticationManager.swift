084cf55c118d92eb237dcdaa557a424aef2dc8e3
Mac McCann <johnwilfredmccann@gmail.com>
2024-12-08 22:55:12 -0500

    fix

--- Heart2Heart/Managers/AuthenticationManager.swift
+++ Heart2Heart/Managers/AuthenticationManager.swift
@@ -87,7 +87,7 @@ class AuthenticationManager: ObservableObject {
                 .document(result.user.uid)
                 .updateData(["fcmToken": fcmToken])
         }
-        // self.isOnboarding = !UserDefaults.standard.bool(forKey: key)
+        self.isOnboarding = !UserDefaults.standard.bool(forKey: key)
         self.user = result.user
     }
     
